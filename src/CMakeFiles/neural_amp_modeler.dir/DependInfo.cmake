4339bddbd0e0d4d5cd38acaa9748d08398fcfb7b
Shaji Khan <quotablecode@gmail.com>
2024-04-17 16:39:27 +0530

    Build for android

---
+++ src/CMakeFiles/neural_amp_modeler.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/deps/NeuralAmpModelerCore/NAM/activations.cpp" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/activations.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/activations.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/deps/NeuralAmpModelerCore/NAM/convnet.cpp" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/convnet.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/convnet.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/deps/NeuralAmpModelerCore/NAM/dsp.cpp" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/dsp.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/dsp.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/deps/NeuralAmpModelerCore/NAM/get_dsp.cpp" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/get_dsp.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/get_dsp.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/deps/NeuralAmpModelerCore/NAM/lstm.cpp" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/lstm.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/lstm.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/deps/NeuralAmpModelerCore/NAM/util.cpp" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/util.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/util.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/deps/NeuralAmpModelerCore/NAM/wavenet.cpp" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/wavenet.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/wavenet.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/src/nam_lv2.cpp" "src/CMakeFiles/neural_amp_modeler.dir/nam_lv2.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/nam_lv2.cpp.o.d"
+  "/home/djshaji/projects/neural-amp-modeler-lv2-amprack/src/nam_plugin.cpp" "src/CMakeFiles/neural_amp_modeler.dir/nam_plugin.cpp.o" "gcc" "src/CMakeFiles/neural_amp_modeler.dir/nam_plugin.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/neural_amp_modeler.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/activations.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/activations.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/convnet.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/convnet.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/dsp.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/dsp.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/get_dsp.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/get_dsp.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/lstm.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/lstm.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/util.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/util.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/wavenet.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/__/deps/NeuralAmpModelerCore/NAM/wavenet.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/nam_lv2.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/nam_lv2.cpp.o.d"
+  "CMakeFiles/neural_amp_modeler.dir/nam_plugin.cpp.o"
+  "CMakeFiles/neural_amp_modeler.dir/nam_plugin.cpp.o.d"
+  "neural_amp_modeler.pdb"
+  "neural_amp_modeler.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neural_amp_modeler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
